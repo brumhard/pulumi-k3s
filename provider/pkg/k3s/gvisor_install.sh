875d8e3513b7a3ef1aaf9754b296959b048c9dbf
Tobias Brumhard <tobias.brumhard@mail.schwarz>
2021-11-21 16:49:08 +0100

    Add gvisor installation script, k3s containerd tpl
    Signed-off-by: Tobias Brumhard <tobias.brumhard@mail.schwarz>

---
+++ provider/pkg/k3s/gvisor_install.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+# copied from https://gvisor.dev/docs/user_guide/install/
+
+set -e
+ARCH=$(uname -m)
+URL=https://storage.googleapis.com/gvisor/releases/release/latest/${ARCH}
+wget ${URL}/runsc ${URL}/runsc.sha512 \
+    ${URL}/containerd-shim-runsc-v1 ${URL}/containerd-shim-runsc-v1.sha512
+sha512sum -c runsc.sha512 \
+    -c containerd-shim-runsc-v1.sha512
+rm -f *.sha512
+chmod a+rx runsc containerd-shim-runsc-v1
+sudo mv runsc containerd-shim-runsc-v1 /usr/local/bin
